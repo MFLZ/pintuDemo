ddcf41e7758d51ec82122827f11ff85b0356f305
MFLZ <253870614@qq.com>
2017-05-10 21:33:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/admin/pintu/ConstantsPool.java
@@ -0,0 +1,20 @@
+package com.example.admin.pintu;
+
+/**
+ * Created by admin on 2017/5/4.
+ */
+
+public final class ConstantsPool {
+    public static final String a = "a";
+    public static final String b = "b";
+    public static final String c = "c";
+
+
+    public interface  model{
+        public static final String x = "x";
+        public static final String y = "y";
+        public static final String z = "z";
+
+    }
+
+}
